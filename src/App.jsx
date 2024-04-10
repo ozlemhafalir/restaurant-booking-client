2037f35eb746b3844da0c723710d331a04dd699f
Özlem Hafalır <ozlemoztep@gmail.com>
2024-04-10 22:35:29 +0200

    Create mockup layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ResponsiveAppBar from "./components/ResponsiveAppBar";
+import {Container, Grid} from "@mui/material";
+import Banner from "./components/Banner.jsx";
+import React from "react";
+import MediaCard from "./components/MediaCard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <Container>
+                <ResponsiveAppBar/>
+            </Container>
+            <Container disableGutters>
+                <Banner/>
+            </Container>
+            <Container>
+                <Grid container>
+                    {Array.from(Array(10).keys()).map((i) =>
+                        <Grid item key={i} sm={3} md={3}>
+                            <MediaCard/>
+                        </Grid>
+                    )}
+                </Grid>
+            </Container>
+        </>
+    )
 }
 
-export default App
+export default App;
---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import {Grid} from "@mui/material";
+
+const Banner = () => {
+    return (
+        <Grid>
+            <img src="https://picsum.photos/1200/500" alt="banner" style={{width: "100%"}} />
+        </Grid>
+    );
+};
+
+export default Banner;
