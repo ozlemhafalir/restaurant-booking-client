cedbf0cc2916be640a05bec60a8c549ee0624404
Özlem Hafalır <ozlemoztep@gmail.com>
2024-10-15 23:04:17 +0200

    add menu to restaurant detail page

--- src/pages/restaurant-detail.jsx
+++ src/pages/restaurant-detail.jsx
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {useParams} from "react-router-dom";
 import Carousel from "../components/Carousel.jsx";
-import {Container, Grid, TextField, Typography} from "@mui/material";
-import RestaurantCard from "../components/RestaurantCard.jsx";
+import {Container, Dialog, Grid, Typography} from "@mui/material";
 import PlaceOutlinedIcon from "@mui/icons-material/PlaceOutlined.js";
 import RestaurantOutlinedIcon from "@mui/icons-material/RestaurantOutlined.js";
 import Maps from "../components/Maps.jsx"
@@ -14,6 +13,7 @@ import Box from "@mui/material/Box";
 import ReservationForm from "../components/ReservationForm.jsx";
 
 const RestaurantDetail = props => {
+    const [menuOpen, setMenuOpen] = useState();
     const {decodedToken} = useJwt(localStorage.getItem('access'));
     const params = useParams()
     const {isPending, error, data} = useQuery({
@@ -23,6 +23,12 @@ const RestaurantDetail = props => {
                 res.data,
             ),
     })
+    const handleMenuClose = () => {
+        setMenuOpen(false);
+    }
+    const openMenuDialog = () => {
+        setMenuOpen(true);
+    }
     return data && (
         <>
             <Container maxWidth={false} disableGutters>
@@ -31,10 +37,16 @@ const RestaurantDetail = props => {
             <Container sx={{mt: 5}}>
                 <Grid container spacing={4}>
                     <Grid item sm={12} md={6}>
-                        <Typography variant={'h2'} sx={{display: "flex", alignItems: "center"}}>
-                            {data.name}<PlaceOutlinedIcon
-                            fontSize={"small"} sx={{ml: 2, mr: 1}}/><small>{data.city_name}</small>
-                        </Typography>
+                        <Box sx={{display: "flex"}}>
+                            <Typography variant={'h2'} sx={{display: "flex", flexGrow: 1, alignItems: "center"}}>
+                                {data.name}
+                                <PlaceOutlinedIcon fontSize={"small"}
+                                                   sx={{ml: 2, mr: 1}}/><small>{data.city_name}</small>
+                            </Typography>
+                            {data.menu != "image/upload/placeholder" && (
+                                <Button onClick={openMenuDialog}>Menu</Button>
+                            )}
+                        </Box>
                         <Typography variant={'h3'} sx={{display: "flex", alignItems: "center"}}>
                             <RestaurantOutlinedIcon fontSize={"small"}
                                                     sx={{marginRight: 1}}/>{data.cuisines.map(item => item.name).join(", ")}
@@ -62,6 +74,16 @@ const RestaurantDetail = props => {
                         <Maps/>
                     </Grid>
                 </Grid>
+                <Dialog
+                    open={menuOpen}
+                    onClose={handleMenuClose}
+                    hasCloseButton
+                    maxWidth={false}
+                >
+                    <Box
+                        component="img"
+                        src={data.menu_url}/>
+                </Dialog>
             </Container>
 
         </>
