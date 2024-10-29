22d93d6fef1ffe0dfac37b059457b287c5db6a77
Özlem Hafalır <ozlemoztep@gmail.com>
2024-10-29 11:15:16 +0100

    Add menu upload in restaurant detail management

--- src/components/RestaurantManagementForm.jsx
+++ src/components/RestaurantManagementForm.jsx
@@ -5,6 +5,9 @@ import PropTypes from "prop-types";
 import {useForm} from "react-hook-form";
 import api from "../api.jsx";
 import Button from "@mui/material/Button";
+import {CloudUpload} from "@mui/icons-material";
+import VisiuallyHiddenInput from "./VisiuallyHiddenInput.jsx";
+import Box from "@mui/material/Box";
 
 const RestaurantManagementForm = ({restaurant, cities, cuisines}) => {
     const [loading, setLoading] = useState(false);
@@ -13,81 +16,108 @@ const RestaurantManagementForm = ({restaurant, cities, cuisines}) => {
         handleSubmit,
         getValues,
         control,
+        setValue,
+        watch,
         formState: {errors},
     } = useForm({
         defaultValues: restaurant
     });
 
+    const watchMenu = watch('menu');
+
     const onSubmit = async (data) => {
         setLoading(true);
-        await api.patch(`/api/owner-restaurant/${restaurant['id']}/`, data).then((res) => {
+        await api.patchForm(`/api/owner-restaurant/${restaurant['id']}/`, data).then((res) => {
             window.location.reload()
         });
         setLoading(false);
     }
+
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-        <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-                <TextField
-                    fullWidth
-                    sx={{my: 1}}
-                    required
-                    id="outlined-required"
-                    label="Restaurant Name"
-                    inputProps={{...register("name")}}
-                />
-                <TextField
-                    fullWidth
-                    sx={{my: 1}}
-                    required
-                    multiline
-                    minRows={3}
-                    id="outlined-required"
-                    label="Restaurant Description"
-                    inputProps={{...register("description")}}
-                />
-                <TextField
-                    fullWidth
-                    sx={{my: 1}}
-                    required
-                    id="outlined-required"
-                    label="Restaurant Address"
-                    inputProps={{...register("address")}}
-                />
-                <FormControl sx={{my: 1}} fullWidth>
-                    <InputLabel id="demo-simple-select-autowidth-label">City</InputLabel>
-                    <Select
-                        labelId="demo-simple-select-autowidth-label"
-                        id="demo-simple-select-autowidth"
-                        label="City"
-                        inputProps={{...register("city")}}
-                        defaultValue={getValues("city")}
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                    <TextField
+                        fullWidth
+                        sx={{my: 1}}
+                        required
+                        id="outlined-required"
+                        label="Restaurant Name"
+                        inputProps={{...register("name")}}
+                    />
+                    <TextField
+                        fullWidth
+                        sx={{my: 1}}
+                        required
+                        multiline
+                        minRows={3}
+                        id="outlined-required"
+                        label="Restaurant Description"
+                        inputProps={{...register("description")}}
+                    />
+                    <TextField
+                        fullWidth
+                        sx={{my: 1}}
+                        required
+                        id="outlined-required"
+                        label="Restaurant Address"
+                        inputProps={{...register("address")}}
+                    />
+                    <FormControl sx={{my: 1}} fullWidth>
+                        <InputLabel id="demo-simple-select-autowidth-label">City</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-autowidth-label"
+                            id="demo-simple-select-autowidth"
+                            label="City"
+                            inputProps={{...register("city")}}
+                            defaultValue={getValues("city")}
+                        >
+                            {cities && cities.results.map(item => (
+                                    <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
+                                )
+                            )}
+                        </Select>
+                    </FormControl>
+                    <MultiSelect items={cuisines.results} label='Cuisines' control={control}/>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    {watchMenu != "image/upload/placeholder" && (
+                        <Box
+                            component="img"
+                            src={typeof watchMenu !== 'string' ? URL.createObjectURL(watchMenu) : restaurant.menu_url}
+                            sx={{maxWidth: "100%"}}
+                        />
+                    )}
+                    <Button
+                        component="label"
+                        role={undefined}
+                        variant="outlined"
+                        tabIndex={-1}
+                        startIcon={<CloudUpload/>}
                     >
-                        {cities && cities.results.map(item => (
-                                <MenuItem key={item.id} value={item.id}>{item.name}</MenuItem>
-                            )
-                        )}
-                    </Select>
-                </FormControl>
-                <MultiSelect items={cuisines.results} label='Cuisines' control={control}/>
-                <Grid container my={2}>
-                    <Grid item xs={12}>
-                        <Button variant="contained" type="submit" disabled={loading} >Save</Button>
-                    </Grid>
+                        Upload files
+                        <VisiuallyHiddenInput
+                            type="file"
+                            onChange={(event) => {
+                                setValue('menu', event.target.files[0]);
+                            }}
+                            name="menu"
+                        />
+                    </Button>
                 </Grid>
             </Grid>
-            <Grid item xs={12} md={6}>
-                menu
+
+            <Grid container my={2}>
+                <Grid item xs={12}>
+                    <Button variant="contained" type="submit" disabled={loading}>Save</Button>
+                </Grid>
             </Grid>
-        </Grid>
         </form>
     );
 };
 
 RestaurantManagementForm.propTypes = {
     restaurant: PropTypes.object.isRequired,
-    data: PropTypes.object.isRequired,
     cities: PropTypes.object.isRequired,
     cuisines: PropTypes.object.isRequired
 }
---
+++ src/components/VisiuallyHiddenInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {styled} from "@mui/material";
+
+const VisiuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+export default VisiuallyHiddenInput;
