908f57efefada6e6e464c86f6b3ebc9ebe065377
Özlem Hafalır <ozlemoztep@gmail.com>
2024-12-03 22:05:07 +0100

    Add pagination to home page

--- src/home.jsx
+++ src/home.jsx
@@ -1,30 +1,51 @@
 import {Container, Grid} from "@mui/material";
 import Banner from "./components/Banner.jsx";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import RestaurantCard from "./components/RestaurantCard";
 import {useQuery} from "@tanstack/react-query";
 import api from "./api.jsx";
+import Button from "@mui/material/Button";
 
 
 function Home() {
-    const {isPending, error, data} = useQuery({
+    const [nextUrl, setNextUrl] = useState('/api/restaurant/');
+    const [results, setResults] = useState([]);
+    const {isPending, error, data, refetch} = useQuery({
         queryKey: ['restaurants'],
         queryFn: () =>
-            api.get('/api/restaurant/').then((res) =>
+            api.get(nextUrl).then((res) =>
                 res.data
             ),
-    })
+    });
+    useEffect(() => {
+        if (data) {
+            setNextUrl(data.next);
+            setResults([...results, ...data.results]);
+        }
+    }, [data])
+
+    const handleClick = () => {
+        refetch();
+    }
     return (
         <>
             <Banner/>
             <Container>
                 <Grid container>
-                    {data?.results?.map((item) =>
+                    {results.map((item) =>
                         <Grid item key={item.id} sm={3} md={3}>
                             <RestaurantCard restaurant={item}/>
                         </Grid>
                     )}
                 </Grid>
+                {nextUrl &&
+                    <Grid container
+                          alignItems="center"
+                          justifyContent="center"
+                          padding={5}>
+                        <Button onClick={handleClick}>Load more!</Button>
+                    </Grid>
+                }
             </Container>
         </>
     )
