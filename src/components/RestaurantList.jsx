8d6fdd2c68d2c87245b2ef9c18f572ff42fe4d55
Özlem Hafalır <ozlemoztep@gmail.com>
2025-04-02 13:08:19 +0200

    loading and no results found messages in restaurantList component

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -3,14 +3,14 @@ import PropTypes from "prop-types";
 import {useQuery} from "@tanstack/react-query";
 import api from "../api.jsx";
 import Banner from "./Banner.jsx";
-import {Container, Grid} from "@mui/material";
+import {Alert, Container, Grid} from "@mui/material";
 import RestaurantCard from "./RestaurantCard.jsx";
 import Button from "@mui/material/Button";
 
 const RestaurantList = ({initialUrl}) => {
     const [nextUrl, setNextUrl] = useState(initialUrl);
     const [results, setResults] = useState([]);
-    const {isPending, error, data, refetch} = useQuery({
+    const {isPending, error, data, refetch, isLoading, isRefetching} = useQuery({
         queryKey: ['restaurants'],
         queryFn: () =>
             api.get(nextUrl).then((res) =>
@@ -30,13 +30,28 @@ const RestaurantList = ({initialUrl}) => {
     return (
         <Container>
             <Grid container marginTop={3}>
-                {results.map((item) =>
+                {(!isLoading && !isRefetching && !results.length) ? (
+                    <Grid container
+                          alignItems="center"
+                          justifyContent="center"
+                          padding={5}>
+                        <Alert variant="outlined" severity="warning">No restaurants found...</Alert>
+                    </Grid>
+                ) : results.map((item) =>
                     <Grid item key={item.id} sm={3} md={3}>
                         <RestaurantCard restaurant={item}/>
                     </Grid>
                 )}
             </Grid>
-            {nextUrl &&
+            {
+                (isPending || isRefetching) ? (
+                    <Grid container
+                          alignItems="center"
+                          justifyContent="center"
+                          padding={5}>
+                        <Button onClick={handleClick} disabled>Loading...</Button>
+                    </Grid>
+                ) : nextUrl &&
                 <Grid container
                       alignItems="center"
                       justifyContent="center"
