5984ecb59eb682d6e9dc78b5c85ad4cf76b69a45
Özlem Hafalır <ozlemoztep@gmail.com>
2025-04-16 13:12:45 +0200

    Distribute restaurant ownerships to different users, correct the restaurant registraion cuisine, add loading... to reservations page

--- src/components/RestaurantRegistrationForm.jsx
+++ src/components/RestaurantRegistrationForm.jsx
@@ -21,7 +21,10 @@ const RestaurantRegistrationForm = ({cities, cuisines}) => {
     }
     const onSubmit = async (data) => {
         setLoading(true);
-        await api.post('/api/register-restaurant/', data).then((res) => {
+        const postData = {...data, ...{
+            cuisines: data.cuisines.map(c => c.id)
+            }};
+        await api.post('/api/register-restaurant/', postData).then((res) => {
             window.location.href = "/account"
         }).catch((err) => {
             setMessage("Error saving registration, please try again later");
--- src/pages/account/reservations.jsx
+++ src/pages/account/reservations.jsx
@@ -1,43 +1,77 @@
-import React, {useMemo} from 'react';
+import React, { useMemo } from 'react';
 import ReservationCard from "../../components/ReservationCard.jsx";
-import {Container} from "@mui/material";
+import { Alert, Container, Grid } from "@mui/material";
 import AccountTabs from "../../components/AccountTabs.jsx";
-import {useQuery} from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import api from "../../api.jsx";
 import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 
 const Reservations = () => {
-    const {isPending, error, data} = useQuery({
+    const {
+        isPending,
+        isRefetching,
+        isLoading,
+        error,
+        data,
+        refetch,
+    } = useQuery({
         queryKey: ['profile-reservations'],
-        queryFn: () =>
-            api.get('/api/profile-reservation/').then((res) =>
-                res.data,
-            ),
+        queryFn: () => api.get('/api/profile-reservation/').then((res) => res.data),
     });
 
     console.log(data?.results)
     const pastReservations = useMemo(() => data?.results?.filter(item=> new Date(item.date)<Date.now()), [data?.results]);
     console.log(pastReservations)
 
-    const upcomingReservations = useMemo(() => data?.results?.filter(item=> new Date(item.date)>=Date.now()), [data]);
-    console.log(upcomingReservations)
+    const upcomingReservations = useMemo(() => (
+        data?.results?.filter(item =>
+            new Date(item.date) >= Date.now() && item.status !== "cancelled"
+        )
+    ), [data?.results]);
+
+    const cancelledReservations = useMemo(() => (
+        data?.results?.filter(item =>
+            item.status === "cancelled"
+        )
+    ), [data?.results]);
+
+
 
     return (
-        <Container sx={{mt: 5}}>
-            <AccountTabs value={0}/>
+        <Container sx={{ mt: 5 }}>
+            <AccountTabs value={0} />
 
-            <Typography variant={'h3'}>
-                Upcoming Reservations
-            </Typography>
-            {upcomingReservations?.map((reservation, i) =>
-                <ReservationCard reservation={reservation} key={i}/>
+            {(isPending || isLoading || isRefetching) && (
+                <Grid container alignItems="center" justifyContent="center" padding={5}>
+                    <Button disabled>Loading...</Button>
+                </Grid>
             )}
-            <Typography variant={'h3'}>
-                Past Reservations
-            </Typography>
-            {pastReservations?.map((reservation, i) =>
-                <ReservationCard reservation={reservation} key={i}/>
-            )}
+
+            {
+                <>
+                    <Typography variant="h3" sx={{ mt: 3 }}>
+                        Upcoming Reservations
+                    </Typography>
+                    {upcomingReservations?.map((reservation, i) => (
+                        <ReservationCard reservation={reservation} key={i} />
+                    ))}
+
+                    <Typography variant="h3" sx={{ mt: 3 }}>
+                        Past Reservations
+                    </Typography>
+                    {pastReservations?.map((reservation, i) => (
+                        <ReservationCard reservation={reservation} key={i} />
+                    ))}
+
+                    <Typography variant="h3" sx={{ mt: 3 }}>
+                        Cancelled Reservations
+                    </Typography>
+                    {cancelledReservations?.map((reservation, i) => (
+                        <ReservationCard reservation={reservation} key={i} />
+                    ))}
+                </>
+            }
         </Container>
     );
 };
